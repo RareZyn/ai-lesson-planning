456347722ae987409a2bd06671dc1881d1ab379a
Razin <razinnazarudin@gmail.com>
2025-05-03 12:25:29 +0800

    basic strucutre

---
+++ frontend/client/src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+
+// Import Bootstrap and Ant Design CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+import "antd/dist/reset.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
